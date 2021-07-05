399a1fa82c72e1552169a02701ab8141fa61a5ec
anuluca <tilucario@outlook.com>
2021-07-05 13:09:23 +0800

    roll

---
+++ src/components/watchKokinggu/index.vue
@@ -0,0 +1,28 @@
+<template src="./index.html" />
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs, onMounted } from "vue";
+
+export default defineComponent({
+  name: "watchKokinggu",
+  setup() {
+    const data: any = reactive({
+      ifAnimation: false,
+
+      //点击硬币
+      roll: () => {
+        data.ifAnimation = true
+        setTimeout(() => {
+          data.ifAnimation = false
+        }, 1000);
+      },
+
+    });
+    return {
+      ...toRefs(data),
+    };
+  },
+});
+</script>
+
+<style lang="scss" src="./index.scss"></style>
